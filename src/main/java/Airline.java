b97bf6ef6a452541823b07196cc96fa165c035c4
Sandra <sandramtzd@hotmail.com>
2023-07-25 15:33:15 +0100

    generating passenger class with its test

--- src/main/java/Airline.java
+++ src/main/java/Airline.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Airline {
+
+    private String airlineName;
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public ArrayList<Flight> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(ArrayList<Flight> flights) {
+        this.flights = flights;
+    }
+
+    private ArrayList<Flight> flights;
+
+
+
+
 }
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,43 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class PassengerTest {
+
+    Passenger passenger;
+
+    @BeforeEach
+
+    public void setUp(){
+
+        passenger = new Passenger ("Joseph Smith", "07852387495", "P001");
+
+    }
+
+    @Test
+    public void canGetPassengerName (){
+        assertThat(passenger.getPassengerName()).isEqualTo("Joseph Smith");
+    }
+
+    @Test
+    public void canSetPassengerName (){
+        passenger.setPassengerName("Sandra");
+        assertThat(passenger.getPassengerName()).isEqualTo("Sandra");
+    }
+
+    @Test
+    public void canGetContactPhone (){
+        assertThat(passenger.getContactPhone()).isEqualTo("07852387495");
+    }
+
+    
+
+
+
+
+
+
+
+
 }
